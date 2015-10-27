2352052159c963eaed2465d670e00455db8ab904
threepears <michael@threepears.com>
2015-10-27 09:43:39 -0500

    Style and structural changes

--- javascripts/app.js
+++ javascripts/app.js
@@ -62,7 +62,7 @@ $(document).ready(function() {
   $(".choice").click(function(e) {
     var x = new window[$(this).children()[1].innerHTML]();
     console.log(x);
-    var message = "<div><h1>" + x.name + "</h1><p>Woody has " + x.healthBonus + " health points." + " He also has " + x.strengthBonus + " strength. To save strength and restore some health, you can choose to hide, but remember ... you only have three hides to use!</p></div>";
+    var message = "<div><h1>" + x.name + "</h1><p>" + x.name + " has " + x.healthBonus + " health points." + " He also has " + x.strengthBonus + " strength. To save strength and restore some health, you can choose to hide, but remember ... you only have three hides to use!</p></div>";
     console.log(message);
     $("#printMessage").append(message);
   });
